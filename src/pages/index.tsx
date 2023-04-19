b48120800772e4b7f7f4f61aa2a977f479727362
Kulunchick <nikolikrasovski@gmail.com>
2023-04-19 11:57:44 +0300

    UPD: Edited main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import PageLayout from "@/components/common/layouts/page-layout";
+
 export default function Home() {
-  return (
-        <>
-          <h1>PetitionApp</h1>
-        </>
-      )
+    return (
+        <PageLayout>
+            <div>
+                <h1>
+                    PetitionApp
+                </h1>
+            </div>
+        </PageLayout>
+    )
 }
