0845f985896bb2408efb0082277a0905d3e935f4
Kulunchick <nikolikrasovski@gmail.com>
2023-04-19 12:02:27 +0300

    UPD: Added ThemeProvider and QueryClientProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {QueryClient, QueryClientProvider} from "react-query";
+import {ThemeProvider} from "@mui/material";
+import theme from "@/styles/theme";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient();
+
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <ThemeProvider theme={theme}>
+            <QueryClientProvider client={queryClient}>
+                <Component {...pageProps} />
+            </QueryClientProvider>
+        </ThemeProvider>
+    )
 }
