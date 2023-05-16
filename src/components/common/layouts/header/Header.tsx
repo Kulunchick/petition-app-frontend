f215ede328fb53059b19287564baf507c5227a1a
Kulunchick <nikolikrasovski@gmail.com>
2023-05-16 22:04:19 +0300

    UPD: Added header

--- src/components/common/layouts/header/Header.tsx
+++ src/components/common/layouts/header/Header.tsx
@@ -1,8 +1,11 @@
-import {AppBar, Box, Toolbar, Typography} from "@mui/material";
+import {AppBar, Box, Button, Toolbar, Typography} from "@mui/material";
 import React from "react";
 import Link from "next/link";
+import {signOut, useSession} from "next-auth/react";
 
 const Header = () => {
+    const {data} = useSession();
+
     return (
         <AppBar
             className="text-black font-semibold"
@@ -13,33 +16,54 @@ const Header = () => {
         >
             <Toolbar
                 disableGutters
-                className="flex justify-around"
+                className="flex xl:justify-around max-xl:justify-between px-4"
             >
                 <Typography
-                    className="font-semibold"
+                    className="font-semibold flex"
                     color="black"
                 >
-                    <span className="text-green-500">Petition</span>App
+                    <Link href={"/"}>
+                        <span className="text-green-500">Petition</span>App
+                    </Link>
                 </Typography>
-                <Box>
+                <Box className="flex">
                     <Link href={'/'}>
                         Home
                     </Link>
                 </Box>
-                <Box className="flex items-center gap-8">
-                    <Link
-                        className="font-semibold"
-                        href={"/login"}
-                    >
-                        Sign In
-                    </Link>
-                    <Link
-                        href={"/register"}
-                        className="border-2 border-green-500 bg-green-500 text-white rounded-full px-5 py-2 inline-block font-semibold hover:bg-white hover:text-green-500"
-                    >
-                        Sign Up
-                    </Link>
-                </Box>
+                {!data && (
+                    <Box className="flex items-center gap-8">
+                        <Link
+                            className="font-semibold"
+                            href={"/login"}
+                        >
+                            Sign In
+                        </Link>
+                        <Link
+                            href={"/register"}
+                            className="border-2 border-green-500 bg-green-500 text-white rounded-full px-5 py-2 inline-block font-semibold hover:bg-white hover:text-green-500"
+                        >
+                            Sign Up
+                        </Link>
+                    </Box>
+                )}
+                {data?.user && (
+                    <Box className="flex items-center gap-8">
+                        <Typography>
+                            {data.user.first_name}
+                        </Typography>
+                        <Button
+                            onClick={() => signOut()}
+                            className="bg-green-500 text-white hover:text-green-500 rounded-full px-5 py-2"
+                            sx={{
+                                border: 2,
+                                borderColor: "green"
+                            }}
+                        >
+                            Logout
+                        </Button>
+                    </Box>
+                )}
             </Toolbar>
         </AppBar>
     )
