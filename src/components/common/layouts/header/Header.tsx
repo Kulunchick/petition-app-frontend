d07a856faf061874f7d9d2db7b640358f58d5fca
Kulunchick <nikolikrasovski@gmail.com>
2023-05-19 00:21:47 +0300

    FIX: Customized header for mobile

--- src/components/common/layouts/header/Header.tsx
+++ src/components/common/layouts/header/Header.tsx
@@ -1,75 +1,150 @@
-import {AppBar, Box, Button, Toolbar, Typography} from "@mui/material";
-import React from "react";
+import {Button, Typography} from "@mui/material";
+import React, {Fragment} from "react";
 import Link from "next/link";
 import {signOut, useSession} from "next-auth/react";
+import {Popover, Transition} from "@headlessui/react";
+import {HiBars3, HiXMark} from "react-icons/hi2";
 
 const Header = () => {
     const {data} = useSession();
 
     return (
-        <AppBar
-            className="text-black font-semibold"
-            sx={{
-                backgroundColor: "#fff"
-            }}
-            position="static"
+        <Popover
+            className="container mx-auto flex items-center border-b-2 px-6 py-2 h-24"
         >
-            <Toolbar
-                disableGutters
-                className="flex xl:justify-around max-xl:justify-between px-4"
+            <Typography
+                className="font-semibold"
+                color="black"
             >
-                <Typography
-                    className="font-semibold flex"
-                    color="black"
+                <Link href={"/"} className="font-semibold text-black">
+                    <span className="text-green-500">Petition</span>App
+                </Link>
+            </Typography>
+            <div className="grow">
+                <div className="hidden sm:flex items-center justify-center gap-2 md:gap-8">
+                    <Link href={"/"}>Home</Link>
+                </div>
+            </div>
+            <div className="flex grow items-center justify-end sm:hidden">
+                <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400
+                hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
+                    <span className="sr-only">Open menu</span>
+                    <HiBars3 className="h-6 w-6" aria-hidden="true"/>
+                </Popover.Button>
+            </div>
+
+            <Popover.Overlay className="fixed inset-0 bg-black opacity-30"/>
+
+            <Transition
+                as={Fragment}
+                enter="duration-200 ease-out"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="duration-100 ease-in"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+            >
+                <Popover.Panel
+                    focus
+                    className="absolute inset-x-0 top-0 origin-top-right transform p-2"
                 >
-                    <Link href={"/"} className="font-semibold text-black">
-                        <span className="text-green-500">Petition</span>App
+                    <div
+                        className="rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 divide-y-2 divide-gray-50">
+                        <div className="px-5 pt-5 pb-6">
+                            <div className="flex items-center justify-between">
+                                <Typography
+                                    className="font-semibold"
+                                    color="black"
+                                >
+                                    <span className="text-green-500">Petition</span>App
+                                </Typography>
+                                <div className="-mr-2">
+                                    <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400
+                                hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
+                                        <span className="sr-only">Open menu</span>
+                                        <HiXMark className="h-6 w-6" aria-hidden="true"/>
+                                    </Popover.Button>
+                                </div>
+                            </div>
+                            <div className="mt-6">
+                                <nav className="grid gap-y-8">
+                                    <Link
+                                        href={"/"}
+                                        className="focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500 px-2"
+                                    >
+                                        Home
+                                    </Link>
+                                </nav>
+                            </div>
+                            <div className="mt-6 flex flex-col items-center gap-2">
+                                {!data ? <div className="w-full flex flex-col items-center gap-2">
+                                    <Link
+                                        href={"/register"}
+                                        className="rounded-md bg-white px-4 py-2 text-sm font-medium text-black md:text-xl w-full
+                                        border-2 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500"
+                                    >
+                                        Sign up
+                                    </Link>
+                                    <Link
+                                        href={"/login"}
+                                        className="rounded-md bg-white px-4 py-2 text-sm font-medium text-black md:text-xl w-full
+                                        border-2 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500"
+                                    >
+                                        Login
+                                    </Link>
+                                </div> : <div className="w-full px-2">
+                                    <Typography className="mb-2">
+                                        {data.user.first_name}
+                                    </Typography>
+                                    <Button
+                                        variant="outlined"
+                                        fullWidth
+                                        color="green"
+                                        onClick={() => signOut()}
+                                        style={{justifyContent: "flex-start"}}
+                                        sx={{
+                                            textTransform: "none",
+                                        }}
+                                    >
+                                        Logout
+                                    </Button>
+                                </div>}
+                            </div>
+                        </div>
+                    </div>
+                </Popover.Panel>
+            </Transition>
+
+            <div className="hidden sm:block">
+                {!data ? <div>
+                    <Link
+                        href={"/register"}
+                        className="mr-4 font-semibold border-2 rounded-md p-2 border-green-500 text-green-500"
+                    >
+                        Sign up
                     </Link>
-                </Typography>
-                <Box className="flex font-semibold text-black">
-                    <Link href={'/'}>
-                        Home
+                    <Link href={"/login"} className="font-semibold">
+                        Login
                     </Link>
-                </Box>
-                {!data && (
-                    <Box className="flex items-center gap-8">
-                        <Link
-                            className="font-semibold text-black"
-                            href={"/login"}
-                        >
-                            Sign In
-                        </Link>
-                        <Link
-                            href={"/register"}
-                            className="border-2 border-green-500 bg-green-500 text-white rounded-full px-5 py-2 inline-block font-semibold hover:bg-white hover:text-green-500"
-                        >
-                            Sign Up
-                        </Link>
-                    </Box>
-                )}
-                {data?.user && (
-                    <Box className="flex items-center gap-8">
-                        <Typography>
-                            {data.user.first_name}
-                        </Typography>
-                        <Button
-                            onClick={() => signOut()}
-                            className="border-2 border-green-500 bg-green-500 text-white rounded-full px-5 py-2 inline-block font-semibold hover:bg-white hover:text-green-500"
-                            sx={{
-                                textTransform: "none",
-                                borderColor: "green",
-                                ":hover": {
-                                    border: 2,
-                                }
-                            }}
-                            variant="outlined"
-                        >
-                            Logout
-                        </Button>
-                    </Box>
-                )}
-            </Toolbar>
-        </AppBar>
+                </div> : <div className="flex items-center gap-x-2">
+                    <Typography>
+                        {data.user.first_name}
+                    </Typography>
+                    <Button
+                        className="font-semibold"
+                        variant="outlined"
+                        fullWidth
+                        color="green"
+                        onClick={() => signOut()}
+                        sx={{
+                            textTransform: "none",
+                        }}
+                    >
+                        Logout
+                    </Button>
+                </div>}
+            </div>
+        </Popover>
     )
 }
 
